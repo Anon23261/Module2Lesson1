9a2e92f494ef1c72cdd1780c429ca1ead6df6613
Anon23261 <anonymous23261@icloud.com>
2024-10-06 05:24:20 -0700

    Update Showcaseyourdancemoves.py
    fixed code

--- Showcaseyourdancemoves.py
+++ Showcaseyourdancemoves.py
@@ -13,7 +13,7 @@ mod = input("How was your day?")
 
 if mood.lower() in ["happy"]:
     print("That's great to hear!")
-elif mood.lower(): == "sad":
+elif mood.lower() == "sad":
     print("I hope your day gets better!")
 else:
     print("Hope things get better!")
