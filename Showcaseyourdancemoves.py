08274967e5e469985cdf4f05d2f96ebfd927b9e6
Anon23261 <anonymous23261@icloud.com>
2024-10-05 21:05:07 -0600

    Add Showcaseyourdancemoves.py

---
+++ Showcaseyourdancemoves.py
@@ -0,0 +1,19 @@
+# Task 1: Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2: Your Mood Today
+
+mod = input("How was your day?")
+
+if mood.lower() in ["happy"]:
+    print("That's great to hear!")
+elif mood.lower(): == "sad":
+    print("I hope your day gets better!")
+else:
+    print("Hope things get better!")
