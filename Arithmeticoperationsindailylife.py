73f13ad83b31630772f5abec2a4dab10eaeb64a3
Anon23261 <anonymous23261@icloud.com>
2024-10-06 00:00:57 -0600

    Corrected naming conventions and added grocery store math and bank interest calculations

---
+++ Arithmeticoperationsindailylife.py
@@ -0,0 +1,16 @@
+bread = 3.18
+peanut_butter = 4.56
+jelly = 3.36
+
+total_cost = bread + peanut_butter + jelly
+
+print("Total cost: $", total_cost)
+
+# Task 2: Bank Interest
+
+initial_amount = 10000
+interest_rate = 0.07
+
+total_amount = initial_amount * (1 + interest_rate)
+
+print("Total amount after one year: $", total_amount)
